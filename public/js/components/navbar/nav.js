6a0fd129f7abcb8dc4896a4c5e72003fc87d321d
RodrigoLago <rodrigazo98@gmail.com>
2021-12-02 23:58:49 -0300

    Actualización nav
    Actualizo el navbar

--- public/js/components/navbar/nav.js
+++ public/js/components/navbar/nav.js
@@ -16,6 +16,51 @@ export const NavSinLogin = () =>`
   </nav>
 `
 export const NavConLogin = (email) =>`
+<nav class="navbar navbar-expand-lg navbar-dark bgcolor" id="navegador">
+        <div class="container-fluid">
+          <a id="imagen-logo" class="navbar-brand logo-empresa" href="../../">
+            <img  src="../img/logo.png" height="80" alt="" loading="lazy" style="margin-top: -1px;"/>
+          </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" id="botonToggle">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0 mx-auto">
+                    <li class="nav-item">
+                      <div class="wrap">
+                        <div class="search">
+                            <input type="text" class="searchTerm" placeholder="¿Que estás buscando?">
+                            <button type="submit" class="searchButton">
+                              <i class="fa fa-search"></i>
+                          </button>
+                        </div>
+                      </div>
+                    </li>
+                </ul>
+            </div>
+            <div id = "margen-barra" class="collapse navbar-collapse" id="navbarSupportedContent">
+              <div class="dropdown">
+                <a id="dropdown-email" class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+                  ${email}
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                  <li><a id="cerrar-sesion" href="" class="dropdown-item" onclick='CerrarSesion()'>Cerrar Sesión</a></li>
+                </ul>
+              </div>
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0 mx-auto">
+              <li class="nav-item">
+              </li>
+              <li class="nav-item">
+                <a id="buy-button" href="../ordenDeCompra">
+                  <img src="../../img/buy.png" alt="imagenBoton"></img>
+                </a>
+              </li>
+            </ul>
+            </div>
+        </div>
+    </nav>
+`
+export const asd =() =>`
 <nav class="navbar navbar-expand-lg navbar-dark bgcolor " id="navegador">
     <div class="container-fluid">
         <a id="imagen-logo" class="navbar-brand logo-empresa" href="../../"><img  src="../img/logo.png" height="80" alt="" loading="lazy" style="margin-top: -1px;"/></a>
@@ -34,7 +79,9 @@ export const NavConLogin = (email) =>`
               <img src="../../img/buy.png" alt="imagenBoton"></img>
             </a>
             <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
-              <li><a id="cerrar-sesion" href="" class="dropdown-item" onclick='CerrarSesion()'>Cerrar Sesión</a></li>
+              <li>
+                <a id="cerrar-sesion" href="" class="dropdown-item" onclick='CerrarSesion()'>Cerrar Sesión</a>
+              </li>
             </ul>
           </li>
         </ul>
@@ -42,7 +89,10 @@ export const NavConLogin = (email) =>`
         </div>
     </div>
   </nav>
-`
+  `
+
+
+
 
 export const NavPrueba = () =>`
   <nav class="navbar navbar-expand-lg navbar-dark bgcolor " id="navegador">
@@ -68,7 +118,6 @@ export const NavPrueba = () =>`
     </div>
   </nav>
 `
-
 
 
 
